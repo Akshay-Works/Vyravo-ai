f7bce5e693645032d49d1dbd4c50b885b3b11390
Akshay Navale <akshay.navale.work@gmail.com>
2026-07-28 12:52:27 +0530

    Add files via upload

---
+++ src/app/crm/page.tsx
@@ -0,0 +1,9 @@
+import { PipelineContent } from "./pipeline-content";
+
+export const metadata = {
+  title: "Pipeline",
+};
+
+export default function PipelinePage() {
+  return <PipelineContent />;
+}
